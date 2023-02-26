62b158abd00e2b652df12f492830f2e87b686533
HasnainRaza <105921794+hasnainraza1212@users.noreply.github.com>
2023-02-26 22:32:58 +0500

    just solved two problems

---
+++ App.js
@@ -0,0 +1,37 @@
+    // task 1
+
+    // var input = prompt("type your string here...", "ghous ahmed saylani");
+    // input contains whole string
+    // var array = input.split(" ");
+    // array contains whole sring 
+    // var checkString = prompt("which substring you want to change " , "saylani")
+    // checkString asks substring to change in input string 
+    // for (var i = 0; i < array.length; i++) {
+    //     if (array[i] === checkString) {
+    //         array[i] = "smit";
+    //     }
+    // }
+    // string contains updated input string
+    // var string = array.join(" ")
+    // console.log(string);
+
+
+    // task 2
+
+// var input1 = prompt("type your string here...", "hello sir how are you??");
+//     // array contains whole string 
+// var array = input1.split(" ");
+//     // array1 contains first letter of every word in upper case
+// var array1 = [];
+//     // array2 contain words after removing first letter
+// var array2 = [];
+//     // array3 contains all words after capitalize
+// var array3 = [];
+// for (var i = 0, j = 0,  k = 0, l = 0; i < array.length, j < array.length, k < array.length, l < array.length; i++, j++, k++, l++) {
+//     array1[j] = array[i].slice(0, 1).toUpperCase();
+//     array2[k] = array[i].slice(1).toLowerCase();
+//     array3[l] = array1[j] + array2[j];
+// }
+//     // array3.join or capitalizeString contains capitalize string
+// var capitalizeString = array3.join(" ");
+// console.log(capitalizeString);
